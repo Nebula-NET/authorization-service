9f339344406a927a2bbf1a4a03c3deb2d1df5e7b
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-12-10 22:06:38 +0330

    create redis service class

---
+++ src/services/redis.service.ts
@@ -0,0 +1,31 @@
+import { RedisClientType } from '@node-redis/client/dist/lib/client';
+import * as redis from 'redis'
+
+
+export class RedisService{
+    private redisClient;
+
+
+    constructor(){
+        (async () =>{
+            const host:string = process.env.REDIS_HOST
+            const port:number = parseInt(process.env.REDIS_PORT)
+    
+            this.redisClient = redis.createClient({
+                url: `redis://${host}:${port}`
+            })
+    
+            await this.redisClient.connect();
+        })()
+    }
+
+    public async set(key: string, value: string){
+        await this.redisClient.set(key, value, {
+            EX: 120
+        })
+    }
+
+    public async get(key: string){
+        return await this.redisClient.get(key)
+    }
+}
