b2bd253d2db6900e3fd075fc762bb5764b79c11e
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-12-10 22:07:05 +0330

    add handle error function

---
+++ src/handlesErrors/handleError.ts
@@ -0,0 +1,29 @@
+import {Response} from 'express'
+import { IResponse } from '../interfaces/response.interface';
+
+export class Exception{
+    public code;
+    public message;
+    constructor(code = 400 , message = 'خطایی پیش آمده، لطفا دوباره تلاش کنید.'){
+        this.code = code;
+        this.message = message
+    }
+}
+
+export function HandleError(res:Response , error){
+    if(error instanceof Exception){
+        let errObj:IResponse = {
+            success: false,
+            message: error.message,
+            data: null
+        }
+        res.status(error.code).json(errObj)
+    }else{
+        let errObj:IResponse = {
+            success: false,
+            message: 'خطایی پیش آمده، لطفا دوباره تلاش کنید.',
+            data: null
+        }
+        res.status(400).json(errObj)
+    }
+}
